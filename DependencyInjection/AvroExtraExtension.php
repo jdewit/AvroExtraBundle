ad6f3ef2c179caa09634fea9a4e93d9f1f34a04b
Joris de Wit <joris.w.dewit@gmail.com>
2012-10-10 02:39:52 -0700

    separate config according to db

--- DependencyInjection/AvroExtraExtension.php
+++ DependencyInjection/AvroExtraExtension.php
@@ -8,17 +8,19 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\Alias;
 
-class AvroExtraExtension extends Extension 
+class AvroExtraExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container) {
-        
+
         $processor = new Processor();
         $configuration = new Configuration();
 
         $config = $processor->processConfiguration($configuration, $configs);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yml');
+
+        $loader->load($config['db_driver']);
+
     }
 }
 
