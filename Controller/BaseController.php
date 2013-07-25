eae233c8ac1f9dacac65a76e5491cd49ae1d8916
Joris de Wit <joris.dewit@wardell.biz>
2013-07-25 14:29:01 -0700

    removed some deps, now only requires bundleAlias and modelAlias as vars

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -5,62 +5,132 @@ namespace Avro\ExtraBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 /**
+ * A base controller class that makes prototyping controllers easy
+ *
+ * REQUIREMENTS
+ * ------------
+ *
+ * You must set the following variables in the child controller
+ *
+ * $modelAlias
+ * $bundleAlias
+ *
  * @author Joris de Wit <joris.w.dewit@gmail.com>
  */
 class BaseController extends Controller
 {
+    /**
+     * The default number of items to show in the list view
+     *
+     * @var int
+     */
+    protected $listCount = 10;
+
+    /**
+     * The models event class
+     *
+     * @var string
+     */
+    protected $eventClass = 'Avro\ExtraBundle\Event\ModelEvent';
+
+    /**
+     * dbDriver
+     *
+     * @var string
+     */
+    protected $dbDriver = 'mongodb';
+
+    /**
+     * The modelManager service id
+     *
+     * The default modelManager is used if set to false
+     *
+     * @var string
+     */
+    protected $modelManager = false;
+
+    /**
+     * List models.
+     *
+     * @Template
+     */
+    public function baseListAction(Request $request)
+    {
+        return $this->get('templating')->renderResponse($this->getTemplate('list'), array(
+           'pagination' => $this->get('knp_paginator')->paginate(
+                               $this->getModelManager()->getQueryBuilder()->hydrate(false)->getQuery(),
+                               $request->query->get('page', 1),
+                               $this->listCount
+                           )
+            )
+        );
+    }
 
     /**
      * Create a new model.
+     *
      */
     public function baseNewAction(Request $request)
     {
         $model = $this->createModel();
 
-        $form = $this->container->get(sprintf('%s.%s.form', $this->alias, $this->modelAlias));
-        $form->setData($model);
+        $form = $this->getForm($model);
 
         if ('POST' === $request->getMethod()) {
             $form->bind($request);
             if ($form->isValid()) {
-                return $this->persistModel($form->getData());
+                $this->persistModel($form->getData());
+
+                return $this->redirect($this->generateUrl(sprintf('%s_%s_list', $this->bundleAlias, $this->modelAlias)));
             }
         }
 
-        return array(
+        return $this->get('templating')->renderResponse($this->getTemplate('new'), array(
             'form' => $form->createview(),
-        );
+            $this->modelAlias => $model
+        ));
     }
 
     /**
      * Base edit action
      */
-    public function baseEditAction(Request $request, $model)
+    public function baseEditAction(Request $request, $id)
     {
-        $form = $this->container->get(sprintf('%s.%s.form', $this->alias, $this->modelAlias));
-        $form->setData($model);
+        $model = $this->getModel($id);
+
+        $form = $this->getForm($model);
 
         if ('POST' === $request->getMethod()) {
             $form->bind($request);
             if ($form->isValid()) {
-                return $this->updateModel($form->getData());
+                $this->updateModel($form->getData());
+
+                return $this->redirect($this->generateUrl(sprintf('%s_%s_list', $this->bundleAlias, $this->modelAlias)));
             }
         }
 
-        return array(
+        return $this->get('templating')->renderResponse($this->getTemplate('edit'), array(
             sprintf('%s', $this->modelAlias) => $model,
             'form' => $form->createview(),
-        );
+        ));
     }
 
     /**
      * Base delete action
+     * @Template
      */
-    public function baseDeleteAction($model)
+    public function baseDeleteAction(Request $request, $id)
     {
         $modelManager = $this->getModelManager();
-        return $modelManager->delete($model);
+
+        $model = $modelManager->getRepository()->find($id);
+
+        $modelManager->delete($model);
+
+        return $this->redirect($this->generateUrl(sprintf('%s_%s_list', $this->bundleAlias, $this->modelAlias)));
     }
 
     protected function setFlash($action, $value)
@@ -68,10 +138,63 @@ class BaseController extends Controller
         $this->get('session')->setFlash($action, $value);
     }
 
+    private function getTemplate($name)
+    {
+        return sprintf('%sBundle:%s:%s.html.twig', str_replace(' ', '', ucwords(str_replace('_', ' ', $this->bundleAlias))), ucfirst($this->modelAlias), $name);
+    }
 
+    /**
+     * getModel
+     *
+     * @param string $id
+     * @return $model
+     */
+    protected function getModel($id)
+    {
+        return $this->getModelManager()->getRepository()->find($id);
+    }
+
+    /**
+     * getModelClass
+     *
+     * @return Model class name
+     */
+    private function getModelClass()
+    {
+        return sprintf('%sBundle\\Document\\%s', str_replace(' ', '\\', ucwords(str_replace('_', ' ', $this->bundleAlias))), ucfirst($this->modelAlias));
+    }
+
+    /**
+     * getModelManager
+     *
+     * @return $modelManager
+     */
     public function getModelManager()
     {
-        return $this->get(sprintf('%s.%s.manager', $this->alias, $this->modelAlias));
+        if ($this->modelManager) {
+            return $this->get($this->modelManager);
+        } else {
+            switch ($this->dbDriver) {
+                case 'mongodb':
+                    $objectManager = 'doctrine.odm.mongodb.document_manager';
+                    $modelManagerClass = 'Avro\ExtraBundle\Document\Manager\DocumentManager';
+                    break;
+                case 'orm':
+                    $objectManager = 'doctrine.orm.entity_manager';
+                    $modelManagerClass = 'Avro\ExtraBundle\ORM\Manager\EntityManager';
+                    break;
+
+            }
+
+            return new $modelManagerClass($this->get($objectManager), $this->getModelClass());
+        }
+    }
+
+    public function getForm($model)
+    {
+        $formType = sprintf('%sBundle\\Form\\Type\\%sFormType', str_replace(' ', '\\', ucwords(str_replace('_', ' ', $this->bundleAlias))), ucfirst($this->modelAlias));
+
+        return $this->createForm(new $formType($this->getModelClass(), $model));
     }
 
     public function createModel()
@@ -79,7 +202,7 @@ class BaseController extends Controller
         $manager = $this->getModelManager();
 
         $model = $manager->create();
-        $this->dispatchEvent(sprintf('%s.%s.created', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.created', $this->bundleAlias, $this->modelAlias), $model);
 
         return $model;
     }
@@ -88,11 +211,11 @@ class BaseController extends Controller
     {
         $manager = $this->getModelManager();
 
-        $this->dispatchEvent(sprintf('%s.%s.persist', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.persist', $this->bundleAlias, $this->modelAlias), $model);
 
         $model = $manager->persist($model);
 
-        $this->dispatchEvent(sprintf('%s.%s.persisted', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.persisted', $this->bundleAlias, $this->modelAlias), $model);
 
         return $model;
     }
@@ -102,9 +225,9 @@ class BaseController extends Controller
     {
         $manager = $this->getModelManager();
 
-        $this->dispatchEvent(sprintf('%s.%s.update', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.update', $this->bundleAlias, $this->modelAlias), $model);
         $manager->update($model);
-        $this->dispatchEvent(sprintf('%s.%s.updated', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.updated', $this->bundleAlias, $this->modelAlias), $model);
 
         return $model;
     }
@@ -113,9 +236,9 @@ class BaseController extends Controller
     {
         $manager = $this->getModelManager();
 
-        $this->dispatchEvent(sprintf('%s.%s.delete', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.delete', $this->bundleAlias, $this->modelAlias), $model);
         $manager->delete($model);
-        $this->dispatchEvent(sprintf('%s.%s.deleted', $this->alias, $this->modelAlias), $model);
+        $this->dispatchEvent(sprintf('%s.%s.deleted', $this->bundleAlias, $this->modelAlias), $model);
 
         return true;
     }
@@ -132,7 +255,7 @@ class BaseController extends Controller
         $model = $this->getModelManager()->getRepository()->find($id);
 
         if (!$model) {
-            $this->get('session')->getFlashBag()->set('error', sprintf('%s.%s.not_found', $this->alias, $this->modelAlias));
+            $this->get('session')->getFlashBag()->set('error', sprintf('%s.%s.not_found', $this->bundleAlias, $this->modelAlias));
 
             return new RedirectResponse($this->container->get('router')->generate('avro_blog_post_list'));
         }
