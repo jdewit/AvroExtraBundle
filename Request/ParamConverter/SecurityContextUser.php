4e96b982046a79d3995d2d085939b8ed54a0985e
Joris de Wit <joris.dewit@wardell.biz>
2013-08-08 17:06:53 -0700

    rename

---
+++ Request/ParamConverter/SecurityContextUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Avro\ExtraBundle\Request\ParamConverter;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+use Avro\ExtraBundle\Event\ParamConverterEvent;
+
+class SecurityContextUser implements ParamConverterInterface
+{
+    private $context;
+
+    public function __construct($context)
+    {
+        $this->context = $context;
+    }
+
+    public function apply(Request $request, ConfigurationInterface $configuration)
+    {
+        $user = $this->context->getToken()->getUser();
+
+        $param = $configuration->getName();
+        $request->attributes->set($param, $user);
+    }
+
+    public function supports(ConfigurationInterface $configuration)
+    {
+        return "FOS\UserBundle\Model\UserInterface" === $configuration->getClass();
+    }
+}
